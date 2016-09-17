c440b08c8e4a5c1bb456299a829955f4a6f49643
ennaangel <chrisvos1992@gmail.com>
2016-09-17 17:00:26 +0200

    updated

---
+++ gw2_calc/js/scripts.js
@@ -0,0 +1,17 @@
+
+	function Costs() {
+		var xhtml = new XMLHttpRequest();
+			xhtml.open("GET", "https://api.guildwars2.com/v2/commerce/prices/19684", true);
+			console.log(xhtml)
+		var gwdat = xhtml.responseText;
+		
+		console.log(gwdat);
+		var = cos = gwdat.sells.unit_price;
+		var = mon = 5* cos;
+		console.log(cos);
+		console.log(mon);
+		return mon;
+	}
+	
+	document.getElementById("moneyz").innerHTML = Costs();
+	

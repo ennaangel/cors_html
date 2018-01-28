4138f02a00beb2f1b64df4f2e0805c1cc586390b
ennaangel <chrisvos1992@gmail.com>
2018-01-28 16:02:47 +0100

    Create scripts.js

---
+++ Inspire/js/scripts.js
@@ -0,0 +1,13 @@
+
+
+var id = "451647"
+var xhttp = new XMLHttpRequest();	
+			xhttp.open("GET", "http://inspirehep.net/search?p=refersto:recid:"+id+"&of=recjson&ot=recid", true);
+			xhttp.send(null); //make request
+			xhttp.onreadystatechange = function() {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {//wait till response
+
+				var idcite 
+        idcite = JSON.parse(xhttp.response); //read in JSON
+        
+        console.log(idcite);

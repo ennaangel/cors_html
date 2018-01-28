3b1eb18c2db28433f15d410489832a4a2c0b2751
ennaangel <chrisvos1992@gmail.com>
2018-01-28 18:08:32 +0100

    Update scripts.js

--- Inspire/js/scripts.js
+++ Inspire/js/scripts.js
@@ -1,8 +1,8 @@
 
-
+//Obtain JSON data
 var id = "451647"
 var xhttp = new XMLHttpRequest();	
-			xhttp.open("GET", "https://inspirehep.net/search?p=refersto:recid:"+id+"&of=recjson&ot=recid", true); //Use https to solve the mixed content problem
+			xhttp.open("GET", "https://inspirehep.net/search?p=refersto:recid:"+id+"&of=recjson&ot=title,recid", true); //Use https to solve the mixed content problem
 			xhttp.send(null); //make request
 			xhttp.onreadystatechange = function() {
 			if (xhttp.readyState == 4 && xhttp.status == 200) {//wait till response
@@ -12,4 +12,20 @@ var xhttp = new XMLHttpRequest();
         
         console.log(idcite);
 			}
-			}
+			};
+
+for (var i = 0; i < idcite.length; i++) {	
+	Create(idcite[i]);
+	
+};
+
+	function Create(idcite){
+		
+		var aDiv = document.createElement('div');
+		var Title = idcite.title.title
+		
+		var atex = document.createTextNode(Title);
+		aDiv.appendChild(atex);
+		
+		document.getElementById("Refs451647").appendChild(aDiv);
+		

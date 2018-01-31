5916561410905b8c05c38fb84fe63aa846d67ccb
ennaangel <chrisvos1992@gmail.com>
2018-01-31 23:19:35 +0100

    Update scripts.js

--- Inspire/js/scripts.js
+++ Inspire/js/scripts.js
@@ -2,6 +2,30 @@
 //Obtain JSON data
 var id = "451647"
 
+//Function that gives the cite list in cites451647
+var xhttp2 = new XMLHttpRequest();	
+	console.log("Doing cite thingy")
+	xhttp2.open("GET", "https://inspirehep.net/record/"+id+"?of=recjson&ot=title,recid,authors,reference", true); //Use https to solve the mixed content problem
+	xhttp2.send(null); //make request
+	xhttp2.onreadystatechange = function() {
+		if (xhttp2.readyState == 4 && xhttp.status == 200) {//wait till response
+		var adcite = JSON.parse(xhttp2.response); //read in JSON
+        	var dcite = adcite.reference
+        	console.log(dcite);
+		
+		var loc2 = "cites451647"
+		var dleng = dcite.length;
+		var dlenn = dleng.toString();
+		var beg = document.createTextNode("This article cites "+dlenn+" times.");
+		document.getElementById(loc2).appendChild(beg);
+		
+		console.log("Starting for loop")
+		for (var i = 0; i < dleng; i++) {	
+			Create(dcite[i],i,loc2);
+			};
+		}
+		};
+
 //Pulling people that refered to article with this id and showing them.
 var xhttp = new XMLHttpRequest();	
 	xhttp.open("GET", "https://inspirehep.net/search?p=refersto:recid:"+id+"&of=recjson&ot=title,recid,authors", true); //Use https to solve the mixed content problem
@@ -26,28 +50,6 @@ var xhttp = new XMLHttpRequest();
 		};
 
 
-//Function that gives the cite list in cites451647
-var xhttp2 = new XMLHttpRequest();	
-	xhttp2.open("GET", "https://inspirehep.net/record/"+id+"?of=recjson&ot=title,recid,authors,reference", true); //Use https to solve the mixed content problem
-	xhttp2.send(null); //make request
-	xhttp2.onreadystatechange = function() {
-		if (xhttp2.readyState == 4 && xhttp.status == 200) {//wait till response
-		var adcite = JSON.parse(xhttp2.response); //read in JSON
-        	var dcite = adcite.reference
-        	console.log(dcite);
-		
-		var loc2 = "cites451647"
-		var dleng = dcite.length;
-		var dlenn = dleng.toString();
-		var beg = document.createTextNode("This article cites "+dlenn+" times.");
-		document.getElementById(loc2).appendChild(beg);
-		
-		console.log("Starting for loop")
-		for (var i = 0; i < dleng; i++) {	
-			Create(dcite[i],i,loc2);
-			};
-		}
-		};
 
 
 

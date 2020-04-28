2326cecd444bb1c23edab846fda7791607fa66e6
ennaangel <chrisvos1992@gmail.com>
2020-04-28 15:57:16 +0200

    no message

--- Python/bibanalysis.py
+++ Python/bibanalysis.py
@@ -1,5 +1,6 @@
 #Chris Vos
 #9 - apr - 2016
+#Chrisvos1992@gmail.com
 #Creating a dict from .bib files
 
 #TODO:  dynamic size for figures, multiple graph styles, network put in seperate function,(SO(N) thing dissapered?)
@@ -19,21 +20,41 @@ import numpy as np
 #max number of displayed histogram things
 #Ignore list
 
+"""
+fn = "fc2"
+bib = "C:/Users/gebruiker/Google Drive/SChorro/Fundamental constants/"+fn+".bib"
 
-fn = "..." #Name of the bib file
-bib = "C:/Users/.../"+fn+".bib" #location of bib file
+fn = "Combined-cLFV"
+bib = "C:/Users/gebruiker/Google Drive/SChorro/Br/BR Eigen/Refs/"+fn+".bib"
+
+fn = "AdsCFTBulkadscftbulk"
+bib = "C:/Users/gebruiker/Google Drive/SChorro/AdsCft/"+fn+".bib"
+
+
+fn = "AdsCFT"
+bib = "D:/Documents/Drive/SChorro/AdsCft/"+fn+".bib"
+"""
+
+fn = "Randomartcont"
+bib = "D:/Documents/Dropbox/andere/Refertielijsten/"+fn+".bib"
 
 max = 50 #Max in histogram
 
 #Define Ignore list
-ignlist = ['the','of','a','and','in','for','with','on','by','an','at','to','from','or','as','have','is','was','it','can','this',"be",'also','these','that','we','which',"has",'find','all','using','between','such','are']
+ignlist = ["but","through","therefore","after","whether","their","than",'the','of','a','and','in','for','with','on','by','an','at','to','from','or','as','have','is','was','it','can','this',"be",'also','these','that','we','which',"has",'find','all','using','between','such','are']
 
 
 
-#--------------Create file to write dict--------------
+#--------------Opening file we want--------------
 print("Opening File "+fn)
 infile = open(bib,encoding="utf-8",errors="ignore")
 analyses = dict()
+
+textfilename = os.path.join("Inoutput", fn+"bibanalyses.txt")
+
+
+#--------------File to print results to
+textfile = open(textfilename,"w+")
 
 #--------------Creating list of entries--------------
 print("Creating list of entries")
@@ -47,6 +68,7 @@ for line in infile:
 		if line.startswith("@"): #creates new entrie at every @
 			i+=1 #keeps track of number of entries
 			entries.append(line)
+			
 		else:
 			entries[i]+= line  #If line does not start iwht @, it ads the next line to current entrie
 	except:
@@ -54,7 +76,7 @@ for line in infile:
 		er1 += 1 #Number of errors
 		continue
 
-#Delete empy entrie
+#Delete empty entrie
 if not entries[0].startswith("@"):
 	del(entries[0])
 
@@ -97,7 +119,7 @@ def leplot(counts,d):
 	ax = fig.add_subplot(111)
 	
 	#colors
-	ax.set_axis_bgcolor("0.3") #Change facecolor
+	ax.set_facecolor("0.3") #Change facecolor
 	fig.patch.set_facecolor("0.3") #Change bg color
 	fig.subplots_adjust(left=0.15,right = 0.95,top=0.93,bottom=0.07)
 	ax.spines['bottom'].set_color('0.3')
@@ -106,8 +128,8 @@ def leplot(counts,d):
 	ax.spines['left'].set_color('0.3')
 	ax.yaxis.label.set_color('0.7')
 	ax.xaxis.label.set_color('0.7')
-	ax.tick_params(axis='x', colors='0.7',top='off',labelsize = 10)
-	ax.tick_params(axis='y', left='off',right="off")
+	ax.tick_params(axis='x', colors='0.7',top=False,labelsize = 10)
+	ax.tick_params(axis='y', left=False,right=False)
 	ax.grid(axis='x',which='major',color='0.6')
 	
 	plt.barh(y_pos,times,color = "palegoldenrod",align="center") #plot
@@ -118,12 +140,13 @@ def leplot(counts,d):
 	else:
 		plt.title("Histogram of publication "+str(d),color="0.7")
 	fig.savefig("Inoutput/"+fn+d+'analyses.png',facecolor="0.3")
+	print("Saving: Inoutput/"+fn+d+'analyses.png')
 	return 
 	
 #Creates Histogram
 def Histo(counts,d):
 	
-	print("Creating histogram of the ",max, " most counted words in ", d)
+	print("Creating histogram of the ",max, " most counted items in ", d)
 	print("From a list of ",len(counts)," different words")
 	if d == "year":
 		t = sorted([ (k,v) for k,v in counts.items()])
@@ -136,32 +159,31 @@ def Histo(counts,d):
 		leplot(t[0:max-1],d)
 	else:
 		leplot(t,d)
-	i=0
-	words = []
-	times = []
-	for v,k in t:
-		#if i < max - 1: #Goes till max number of words is reahced
-		#if v > 12: #Goes till counts is larger than 10, chnging this has major influences of speed of the network loop
-		
-		try: 
-			#print(k,": ",v)
-			words.append(k)#appends the word to word list
-			times.append(v)#appends the time the word was used to list
-			i+=1
-		except:
-			print("---Error on printing the word---")
+	return t
 
-		return  [words,times]
-
+#Wtites histogram to file
+def writehisto(item,items,fname):
+        print("Writing histo of "+item)
+        fname.write("List of "+item+"\n")
+        stringofitems = "String of "+item+": "
+        if item != "years":
+                for item in items:
+                        fname.write(item[1]+": "+str(item[0])+"\n")
+                        stringofitems+="\""+item[1]+"\","
+        else:
+                for item in items:
+                        fname.write(str(item[0])+": "+str(item[1])+"\n")
+        fname.write(stringofitems)
+        fname.write("\n--------------\n\n")
 			
 #--------------Finding titles and building the Dictionary--------------
 print("Entries are seperated, time to disect")
 for entrie in entries:
 	entrynum += 1
 	erors = False
-	
+
 	#Entrie Title
-	tit = re.search("Title(.)+?},\n",entrie,re.DOTALL)
+	tit = re.search("title(.)+?},\n",entrie,re.DOTALL)
 	if tit != None:
 		tit = str(tit.group()).replace("\n","").strip("Title").strip(" ").strip("= ").replace("{","").replace("},","").replace("}","")#Tidy up strings
 		print(tit.encode("utf8","ignore"))
@@ -184,9 +206,9 @@ for entrie in entries:
 	analyses[entrynum]["Type"] = type
 
 	#Authors
-	authors = re.search("Author(.)+?},\n",entrie,re.DOTALL)
+	authors = re.search("author(.)+?},\n",entrie,re.DOTALL)
 	if authors != None:
-		authors = str(authors.group()).strip("Author").strip("\n").strip(" ").strip("= ").replace("{","").strip("},").replace("}","")
+		authors = str(authors.group()).strip("author").strip("\n").strip(" ").strip("= ").replace("{","").strip("},").replace("}","")
 		authors = authors.split(" and ")
 		#print(authors)
 	else:
@@ -198,11 +220,12 @@ for entrie in entries:
 	analyses[entrynum]["Authors"] = authors
 			
 	#Year
-	year = re.search("Year(.)+?}",entrie)
+	year = re.search("  year(.)+?}",entrie)
+	
 	if year != None :
-		year = str(year.group()).strip("\n").strip("Year").strip(" ").strip("= ").replace("{","").strip("},").replace("}","")
+		year = str(year.group()).strip("\n").strip().strip("year").strip(" ").strip("= ").replace("{","").strip("},").replace("}","")
 		year = int(year.strip())
-		#print(year)
+		
 	else:
 		nyear += 1
 		nlyear.append(entrynum)
@@ -212,11 +235,11 @@ for entrie in entries:
 	analyses[entrynum]["Year"] = year
 	
 	#Abstract
-	abstract = re.search("Abstract(.)+?},\n",entrie,re.DOTALL)
+	abstract = re.search("abstract(.)+?},\n",entrie,re.DOTALL)
 	
 	
 	if abstract != None :
-		abstract = str(abstract.group()).strip("\n").strip("Abstract").strip(" ").strip("= ").replace("{","").strip("},").replace("}","")
+		abstract = str(abstract.group()).strip("\n").strip("abstract").strip(" ").strip("= ").replace("{","").strip("},").replace("}","")
 		analyses[entrynum]["Abscounts"] = counter(abstract)
 	else: #Not added to nentrie, becaus many articles don't have an Abstract
 		nabstract += 1
@@ -256,6 +279,7 @@ for entrie in analyses:
 			ftitcount[key] = ftitcount.get(key,0)+1
 #print(ftitcount)		
 Tithis = Histo(ftitcount,"titles")#Creates histogram, and returns list with [words,number,plut]
+writehisto("titles",Tithis,textfile)
 print("---------------------")
 #On Abstracts
 print("Abstracts")
@@ -265,7 +289,8 @@ for entrie in analyses:
 		for key in analyses[entrie]["Abscounts"]:
 			fabscount[key] = fabscount.get(key,0)+1
 #print(ftitcount)		
-Abshis = Histo(fabscount,"abstracts")	#Creates histogram, and returns list with [words,number,plut]
+Abshis = Histo(fabscount,"abstracts")	#Creates histogram, and returns list with [words,number]
+writehisto("abstracts",Abshis,textfile)
 print("---------------------")
 #On years
 print("year")
@@ -276,7 +301,8 @@ for entrie in analyses:
 		#print(key)
 		ycount[key] = ycount.get(key,0)+1		
 #print(ycount)		
-Yhis = Histo(ycount,"year")	#Creates histogram, and returns list with [year,number,plut]
+Yhis = Histo(ycount,"year")	#Creates histogram, and returns list with [year,number]
+writehisto("years",Yhis,textfile)
 print("---------------------")
 #On Authors
 print("Authors")
@@ -286,7 +312,9 @@ for entrie in analyses:
 		for key in analyses[entrie]["Authors"]:
 			fautscount[key] = fautscount.get(key,0)+1		
 #print(fautcount)		
-Abshis = Histo(fautscount,"authors")	#Creates histogram, and returns list with [words,number,plut]
+Authis = Histo(fautscount,"authors")	#Creates histogram, and returns list with [words,number]
+writehisto("authors",Authis,textfile)
+
 
 #start of network: remove and put seperate for optimalizaziotn
 #Creating Author list
@@ -322,4 +350,5 @@ plt.show()	#Displays plots
 file = os.path.join("Inoutput", fn+"bibanalyses.p")
 pickle.dump(analyses, open(file, "wb" ))
 
+textfile.close()
 infile.close()
